08512948ca726a4f83af73b0cc357d7fcc85ccf0
Melese Tamiru <mtamiru@psi-e.org>
2020-09-12 11:37:31 +0300

    orderd by

--- home/views.py
+++ home/views.py
@@ -13,17 +13,17 @@ def album_detail(request,album_id):
 
 def recent_releases(request):
     albums = Music.objects.filter(release_year__gte = 2008)
-    albums = albums.order_by('release_year')
+    albums = albums.order_by('-release_year')
     return render(request, 'home/recent_releases.html', {'albums': albums})
 
 def popular_albums(request):
-    albums = Music.objects.filter(is_popular__exact = True)
+    albums = Music.objects.filter(is_popular__exact = True).order_by('-release_year')
     return render(request, 'home/popular_albums.html', {'albums': albums})
 def community_songs(request):
     albums = Music.objects.filter(is_community_song__exact=True)
     return render(request, 'home/community_songs.html', {'albums': albums})
 def all_albums(request):
-    albums = Music.objects.filter(is_album__exact=True)
+    albums = Music.objects.filter(is_album__exact=True).order_by('-release_year')
     return render(request, 'home/home.html', {'albums': albums})
 
 def interviews(request):
